1ce33762f9e3a1872c8b85eb3773f13cf5e7f29a
Bulat Tkebuchava <93533750+batat106@users.noreply.github.com>
2025-06-21 01:11:20 +0300

    2nd commit

---
+++ SocialNetwork/SocialNetwork/Core/SocialNetwork309.Application/Features/Friends/Queries/GetFriends/GetFriendsQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using SocialNetwork309.Application.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork309.Application.Features.Friends.Queries.GetFriends
+{
+    /*public class GetFriendsQueryHandler : IRequestHandler<GetFriendsQuery, List<IdentityUser>>
+    {
+        private readonly IFriendService _friendService;
+
+        public GetFriendsQueryHandler(IFriendService friendService)
+        {
+            _friendService = friendService;
+        }
+
+        public async Task<List<IdentityUser>> Handle(GetFriendsQuery query, CancellationToken cancellationToken)
+        {
+            return await _friendService.GetFriendsAsync(query.UserId);
+        }
+    }*/
+}
---
+++ SocialNetwork/SocialNetwork/Core/SocialNetwork309.Application/Interfaces/Repositories/IMessageRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork309.Application.Interfaces.Repositories
+{
+    public class IMessageRepository
+    {
+
+    }
+}
